6c7a04bdb47a937cabbfdcd968948025891a82ab
thongvm-sync-partners <thongvm@sync-partners.com>
2023-12-04 11:05:50 +0900

    update moromi.js

--- src/pages/moromi.js
+++ src/pages/moromi.js
@@ -5,49 +5,49 @@ import { parseToLineArray } from "../help/parseToLineArray";
 import DataTable from "../components/data-table/data-table";
 function Moromi() {
   const [data, setData] = useState(moromiMock);
-  const numbersArray = Array.from(
-    { length: data.length },
-    (_, index) => index + 1
-  );
+
   const [dataLineChart, setDataLineChart] = useState({
-    day: numbersArray,
+    day: [],
     line: parseToLineArray(data),
   });
+  const changeData = () => {
+    setData([
+      {
+        id: 1,
+        day: 1,
+        time: "2022-12-01 05:00:00",
+        roomTemperature: null,
+        productTemperature: 7,
+        baume: null,
+        japanSakeLevel: null,
+        alcoholContent: null,
+      },
+      {
+        id: 2,
+        day: 2,
+        time: "2022-12-01 05:00:00",
+        roomTemperature: 5,
+        productTemperature: 7.5,
+        baume: 6.7,
+        japanSakeLevel: null,
+        alcoholContent: 2.8,
+      },
+    ]);
+  };
   useEffect(() => {
-    console.log("dataLineChart :", dataLineChart);
-    console.log("numbersArray :", numbersArray);
-  }, [data, numbersArray, dataLineChart]);
+    const numbersArray = Array.from(
+      { length: data.length },
+      (_, index) => index + 1
+    );
+    setDataLineChart({
+      day: numbersArray,
+      line: parseToLineArray(data),
+    });
+  }, [data]);
   return (
     <div>
-      <button
-        onClick={() => {
-          setData([
-            {
-              id: 1,
-              day: 1,
-              time: "2022-12-01 05:00:00",
-              roomTemperature: null,
-              productTemperature: 7,
-              baume: null,
-              japanSakeLevel: null,
-              alcoholContent: null,
-            },
-            {
-              id: 2,
-              day: 2,
-              time: "2022-12-01 05:00:00",
-              roomTemperature: 5,
-              productTemperature: 7.5,
-              baume: 6.7,
-              japanSakeLevel: null,
-              alcoholContent: 2.8,
-            },
-          ]);
-        }}
-      >
-        change
-      </button>
-      <Chart dataLineChart={dataLineChart} />
+      <button onClick={changeData}>change</button>
+      {dataLineChart.day.length && <Chart dataLineChart={dataLineChart} />}
       <DataTable data={data} />
     </div>
   );
