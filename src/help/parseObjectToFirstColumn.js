51618088975902c1a37a7b6be56191bb372d91cb
thongvm-sync-partners <thongvm@sync-partners.com>
2023-12-04 10:48:41 +0900

    create 1 file and update 1 file

---
+++ src/help/parseObjectToFirstColumn.js
@@ -0,0 +1,3 @@
+export const parseObjectToFirstColumn = (array) => {
+  Object.keys(array[0]).forEach((key) => {});
+};
--- src/pages/moromi.js
+++ src/pages/moromi.js
@@ -16,6 +16,43 @@ function Moromi() {
   return (
     <div>
       <Chart dataLineChart={dataLineChart} />
+      <div style={{ display: "flex" }}>
+        <table>
+          <tbody>
+            {Object.keys(data[0]).map((name, index) => {
+              return (
+                <tr key={index}>
+                  <td
+                    style={{
+                      position: "sticky",
+                      top: 0,
+                      backgroundColor: "white",
+                      zIndex: 1,
+                    }}
+                  >
+                    {name}
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+        <div style={{ overflowX: "auto", maxWidth: "100%" }}>
+          <table>
+            <tbody>
+              {Object.keys(data[0]).map((name, index) => {
+                return (
+                  <tr key={index}>
+                    {data.map((column) => {
+                      return <td>{column[name]}</td>;
+                    })}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
