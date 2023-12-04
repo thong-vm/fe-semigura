556539817d24bb6447523181623dc75ab98fe110
thongvm-sync-partners <thongvm@sync-partners.com>
2023-12-04 14:34:14 +0900

    update main.js

--- src/layout/main.js
+++ src/layout/main.js
@@ -1,9 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Moromi from "../pages/moromi/moromi";
+import Dashboard from "../pages/dashboard/dashboard";
 function Main() {
   return (
     <Routes>
-      <Route path="/" element={<Moromi />} />
+      <Route path="/" element={<Dashboard />} />
       <Route path="/moromi" element={<Moromi />} />
     </Routes>
   );
---
+++ src/pages/dashboard/dashboard.js
@@ -0,0 +1,12 @@
+import Moromi from "../moromi/moromi";
+
+function Dashboard() {
+  return (
+    <div>
+      <Moromi />
+      {/* orther table  */}
+    </div>
+  );
+}
+
+export default Dashboard;
