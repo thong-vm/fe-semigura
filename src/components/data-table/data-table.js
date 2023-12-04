3ff0f99909806339eccd472bb0fe18f1094eabbc
thongvm-sync-partners <thongvm@sync-partners.com>
2023-12-04 11:00:26 +0900

    create 1 file and update 1 file

---
+++ src/components/data-table/data-table.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+const DataTable = ({ data }) => {
+  return (
+    <div style={{ display: "flex" }}>
+      <table>
+        <tbody>
+          {Object.keys(data[0]).map((name, index) => (
+            <tr key={index}>
+              <td
+                style={{
+                  position: "sticky",
+                  top: 0,
+                  backgroundColor: "white",
+                  zIndex: 1,
+                }}
+              >
+                {name}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div style={{ overflowX: "auto", maxWidth: "100%" }}>
+        <table>
+          <tbody>
+            {Object.keys(data[0]).map((name, index) => (
+              <tr key={index}>
+                {data.map((column) => (
+                  <td key={column.id}>{column[name]}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default DataTable;
--- src/pages/moromi.js
+++ src/pages/moromi.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import Chart from "../components/chart/chart";
 import { moromiMock } from "../mock/moromi";
 import { parseToLineArray } from "../help/parseToLineArray";
+import DataTable from "../components/data-table/data-table";
 function Moromi() {
   const [data, setData] = useState(moromiMock);
   const numbersArray = Array.from(
@@ -12,47 +13,42 @@ function Moromi() {
     day: numbersArray,
     line: parseToLineArray(data),
   });
-  useEffect(() => {}, [data, numbersArray, dataLineChart]);
+  useEffect(() => {
+    console.log("dataLineChart :", dataLineChart);
+    console.log("numbersArray :", numbersArray);
+  }, [data, numbersArray, dataLineChart]);
   return (
     <div>
+      <button
+        onClick={() => {
+          setData([
+            {
+              id: 1,
+              day: 1,
+              time: "2022-12-01 05:00:00",
+              roomTemperature: null,
+              productTemperature: 7,
+              baume: null,
+              japanSakeLevel: null,
+              alcoholContent: null,
+            },
+            {
+              id: 2,
+              day: 2,
+              time: "2022-12-01 05:00:00",
+              roomTemperature: 5,
+              productTemperature: 7.5,
+              baume: 6.7,
+              japanSakeLevel: null,
+              alcoholContent: 2.8,
+            },
+          ]);
+        }}
+      >
+        change
+      </button>
       <Chart dataLineChart={dataLineChart} />
-      <div style={{ display: "flex" }}>
-        <table>
-          <tbody>
-            {Object.keys(data[0]).map((name, index) => {
-              return (
-                <tr key={index}>
-                  <td
-                    style={{
-                      position: "sticky",
-                      top: 0,
-                      backgroundColor: "white",
-                      zIndex: 1,
-                    }}
-                  >
-                    {name}
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-        <div style={{ overflowX: "auto", maxWidth: "100%" }}>
-          <table>
-            <tbody>
-              {Object.keys(data[0]).map((name, index) => {
-                return (
-                  <tr key={index}>
-                    {data.map((column) => {
-                      return <td>{column[name]}</td>;
-                    })}
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <DataTable data={data} />
     </div>
   );
 }
