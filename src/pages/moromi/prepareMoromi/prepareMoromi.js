fa851ab06c422f1a4627810448c7494fba5abd35
Van-vuhong <vanvh@sync-partners.com>
2023-12-05 14:48:19 +0900

    edit

--- src/pages/moromi/prepareMoromi/prepareMoromi.js
+++ src/pages/moromi/prepareMoromi/prepareMoromi.js
@@ -1,16 +1,17 @@
-import { memo } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import PageTable from "../../../components/page-table/page-table";
-import { selectAllPrepareMoromi, updatePrepareMoromi } from "../../../store/moromi/prepareMoromiSlice";
+import {
+  selectAllPrepareMoromi,
+  updatePrepareMoromi,
+} from "../../../store/moromi/prepareMoromiSlice";
 
 function PrepareMoromi() {
-    const dispatch = useDispatch();
-    const data = useSelector(selectAllPrepareMoromi);
-    const updateData = ({ id, changes }) => {
-      dispatch(updatePrepareMoromi({ id, changes }));
-    };
-    const MemoizedPageTable = memo(PageTable);
-    return <MemoizedPageTable data={data} updateData={updateData} />;
+  const dispatch = useDispatch();
+  const data = useSelector(selectAllPrepareMoromi);
+  const updateData = ({ id, changes }) => {
+    dispatch(updatePrepareMoromi({ id, changes }));
+  };
+  return <PageTable data={data} updateData={updateData}   axis={{ x: "Time", y: "t°" }} />;
 }
 
 export default PrepareMoromi;
