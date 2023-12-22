b036a1fb6749b9ec637c688ceaa460d229a31fab
thong-vm <thongvm@sync-partners.com>
2023-12-22 16:35:21 +0900

    update ComboBox.js and Moromi.module.css

--- src/components/ComboBox/ComboBox.js
+++ src/components/ComboBox/ComboBox.js
@@ -2,10 +2,11 @@ import * as React from "react";
 import TextField from "@mui/material/TextField";
 import Autocomplete from "@mui/material/Autocomplete";
 import classes from "./ComboBox.module.css";
+import { useState } from "react";
 
 export default function ComboBox({ label, dataSource, handleOutput }) {
-  const [value, setValue] = React.useState(dataSource[0]);
-  const [inputValue, setInputValue] = React.useState("");
+  const [value, setValue] = useState(dataSource[0]);
+  const [inputValue, setInputValue] = useState("");
   return (
     <div className={classes.comboBox}>
       <label className={classes.comboBoxLabel}>{label} </label>
